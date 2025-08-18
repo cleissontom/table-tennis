234ecc2da5f3e85aeb695b41d6067995f10e4f0f
Cleisson <cavalcante.cleisson@gmail.com>
2025-08-17 21:17:22 -0300

    ajustando layout da pagina com html

--- app.py
+++ app.py
@@ -14,18 +14,28 @@ DB_PORT = st.secrets["db"]["port"]
 engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}")
 
 
+def color_rows(row):
+    return ['background-color: #f2f2f2' if row.name % 2 == 0 else 'background-color: #ffffff'] * len(row)
+
+
+
+
 st.header('Resultado Geral')
 nome_tabela = 'vw_total_geral'
 df = pd.read_sql(f"SELECT * FROM {nome_tabela} ;", engine)
-st.dataframe(df)
+st.dataframe(df,hide_index=True)
+
 
 st.subheader('Resultado ultimos dias')
 nome_tabela = 'vw_ultimos_resultados_diarios'
 df = pd.read_sql(f"SELECT * FROM {nome_tabela} ;", engine)
-st.dataframe(df)
+st.dataframe(df,hide_index=True)
 
-st.subheader('Resultado ultimos apostas')
+st.subheader('Resultado ultimas apostas')
 nome_tabela = 'vw_ultimos_resultados'
 df = pd.read_sql(f"SELECT * FROM {nome_tabela} ;", engine)
-st.dataframe(df,width=2000)
+st.markdown(
+    df.to_html(),
+    unsafe_allow_html=True
+)
 

e7b4de795a5e18c1717cba7a657e2f6a4c8dde5e
Cleisson <cavalcante.cleisson@gmail.com>
2025-08-18 22:05:11 -0300

    alterando visualizacao das tabelas do streamlit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import psycopg2
 import streamlit as st
 from sqlalchemy import create_engine
-
+#from st_aggrid import AgGrid
 
 # 🔹 Configuração da conexão (troque pelos seus dados)
 DB_HOST = st.secrets["db"]["host"]
@@ -14,28 +14,25 @@ DB_PORT = st.secrets["db"]["port"]
 engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}")
 
 
-def color_rows(row):
-    return ['background-color: #f2f2f2' if row.name % 2 == 0 else 'background-color: #ffffff'] * len(row)
-
-
-
-
 st.header('Resultado Geral')
 nome_tabela = 'vw_total_geral'
 df = pd.read_sql(f"SELECT * FROM {nome_tabela} ;", engine)
-st.dataframe(df,hide_index=True)
+styled_df = df.style.set_properties(**{'text-align': 'center'})
+st.write(styled_df.to_html(), unsafe_allow_html=True)
+
 
 
 st.subheader('Resultado ultimos dias')
 nome_tabela = 'vw_ultimos_resultados_diarios'
-df = pd.read_sql(f"SELECT * FROM {nome_tabela} ;", engine)
-st.dataframe(df,hide_index=True)
+df = pd.read_sql(f"SELECT * FROM {nome_tabela}", engine)
+styled_df = df.style.set_properties(**{'text-align': 'center'})
+st.write(styled_df.to_html(), unsafe_allow_html=True)
+#st.dataframe(df, hide_index=True)
+
+
 
 st.subheader('Resultado ultimas apostas')
-nome_tabela = 'vw_ultimos_resultados'
-df = pd.read_sql(f"SELECT * FROM {nome_tabela} ;", engine)
-st.markdown(
-    df.to_html(),
-    unsafe_allow_html=True
-)
+nome_tabela = 'resultados_base_teste'
+df = pd.read_sql(f"SELECT * FROM {nome_tabela} order by data_jogo desc limit 20;", engine)
+st.dataframe(df, hide_index=True)
 
